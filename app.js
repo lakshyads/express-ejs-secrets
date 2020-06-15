421a46036f13ae636ef018148f6c78a00954d53f
Lakshya Dev <lakshyadev@live.com>
2020-06-15 15:52:42 +0530

    Attached MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 import express from 'express';
 import ejs from 'ejs';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -11,6 +12,17 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
+const dbName = process.env.DB_Name || 'secretsDB';
+const dbString = process.env.DB_STRING || `mongodb://localhost:27017/${dbName}`;
+
+// Connect DB
+mongoose.connect(dbString, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if (err)
+        console.log(`Error connecting to DB: `, err);
+    else
+        console.log(`Connected to MongoDB`);
+
+});
 
 
 app.get('/', (req, res) => {
